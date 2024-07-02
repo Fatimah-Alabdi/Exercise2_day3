3e7a544c1f9901c2fecc02900800c15ab80ba3bc
Fatimah-Alabdi <85888515+Fatimah-Alabdi@users.noreply.github.com>
2024-07-02 16:00:15 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,139 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//1.Write a program that checks the role of the user
+        /*String role;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your role: ");
+        role = scanner.nextLine();
+        if (role.equalsIgnoreCase("admin")) {
+            System.out.println("Welcome Admin!");
+
+        }
+        else if (role.equalsIgnoreCase("superuser")) {
+            System.out.println("Welcome Superuser!");
+        }
+        else if (role.equalsIgnoreCase("user")) {
+            System.out.println("Welcome User!");
+
+         */
+
+
+
+
+
+        //2.Take three numbers from the user and print the greatest number.
+       /* Scanner scanner = new Scanner(System.in);
+        int number1;
+        int number2;
+        int number3;
+        System.out.println("enter number1");
+        number1 = scanner.nextInt();
+        System.out.println("enter number2");
+        number2 = scanner.nextInt();
+        System.out.println("enter number3");
+        number3 = scanner.nextInt();
+
+        if (number1 > number2 && number1>number3) {
+            System.out.println(number1 + " is greatest number");
+
+        }else if (number2 > number1 && number2>number3) {
+            System.out.println(number2 + " is greatest number");
+        }else
+            System.out.println(number3 + " is greatest number");
+
+
+        */
+
+
+        //3.Write a Java program that generates an integer between 1 and 7 and displays the name of the weekday.
+        /*Scanner scanner = new Scanner(System.in);
+        int num;
+        System.out.println("Enter the number: ");
+        num = scanner.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("sunday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+                case 3:
+                    System.out.println("tuesday");
+                    break;
+                    case 4:
+                        System.out.println("wednesday");
+                        break;
+                        case 5:
+                            System.out.println("thursday");
+                            break;
+                            case 6:
+                                System.out.println("friday");
+                                break;
+                                case 7:
+                                    System.out.println("saturday");
+                                    break;
+            default:
+                System.out.println("Invalid number, you must enter number between 1 and 7");
+
+         */
+
+        //4. Write a program that takes a numeric score as input and prints the corresponding letter grade using the following grading scale:
+
+        /*Scanner scanner = new Scanner(System.in);
+        int grade;
+        System.out.println("Please enter your grade : ");
+        grade = scanner.nextInt();
+        System.out.println("numeric score :"+ grade);
+
+        if (grade >= 90 && grade <= 100) {
+            System.out.printf("letter grade : A");
+        }
+        else if (grade >= 80 && grade <= 90) {
+            System.out.printf("letter grade : B");
+        }
+        else if (grade >= 70 && grade <= 80) {
+            System.out.printf(" letter grade : C");
+        }
+        else if (grade >= 60 && grade <= 70) {
+            System.out.printf("letter grade : D");
+        }
+        else if ( grade<60) {
+            System.out.printf("letter grade : f ");
+
+         */
+       // 5. Write a Java program that takes a person's age as input and categorizes them into one of three age categories: "Child," "Teenager," or "Adult" using an if statement.
+       /* Scanner scanner = new Scanner(System.in);
+        int age;
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+
+        if (age < 13) {
+            System.out.println("You are a child!");
+
+
+        }
+        else if (age >= 13 && age <=19) {
+            System.out.println("You are a Teenager!");
+        }
+        else if (age >20)
+
+    {
+        System.out.println(" you are an Adult");
+    }
+
+
+        */
+
+
+
+        }
+        }
+
+
+
+
+
+
+
